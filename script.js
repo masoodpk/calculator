0b839698959f2d81978ab7fc0200997d1c9413de
masood <masoodpk555@gmail.com>
2023-11-06 11:29:39 +0530

    function answer added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let screen = document.getElementById("screen");
  function clickHandler(sign){
     screen.value += sign;
-    
+
+ }
+ function answer(){
+    screen.value = eval(screen.value);
  }
